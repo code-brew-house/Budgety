627a017305e0be33c774c85bae8b2154cdcde1ad
Tushar Bharadwaj <tech.codebrewhouse@gmail.com>
2026-02-09 14:12:56 +0530

    feat: configure NativeWind and Gluestack UI

---
+++ apps/mobile/tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
